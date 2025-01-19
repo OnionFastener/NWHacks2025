a2f3845f25c55d7ea80db9dba06be52637c73d32
owen song <owensong.ca@gmail.com>
2025-01-19 05:09:55 -0800

    welcomed card

--- config/frontend/src/Sidebar.js
+++ config/frontend/src/Sidebar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './styles/Sidebar.css';
 
-const Sidebar = () => {
+import ButtonComponent from "./ButtonComponent.js";
+
+const Sidebar = ({ togglePopup, showPopup }) => {
     return (
         <div className="sidebar">
             <h1 className="app-title">John's Health Companion</h1>
+
+            <ButtonComponent togglePopup={togglePopup} />
+            
             <button className="action-button">Medical History</button>
             <button className="action-button">Book Appointment</button>
             <button className="new-chat-button">+ New Chat</button>
